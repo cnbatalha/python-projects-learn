c89816adc593f2b1cb2cc0e65b711aef910f4d3b
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-28 21:35:33 -0400

    add testes autonomos

--- django/agenda/myagenda/polls/tests.py
+++ django/agenda/myagenda/polls/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import datetime 
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionMethodTests(TestCase):
+    
+    def test_was_pubblished_recently_future_question(self):
+        '''
+        '''
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
